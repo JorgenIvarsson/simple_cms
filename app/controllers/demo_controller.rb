0788b7013b918cb83f146f4d9081494b588dd283
Jörgen Ivarsson <jorgen.ivarsson@gmail.com>
2018-09-19 21:31:39 +0200

    Initial commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,16 @@
+class DemoController < ApplicationController
+  layout false
+
+  def index
+  end
+
+  def hello
+    @array = [1,2,3,4,5]
+    render('hello')
+  end
+
+  def other_hello
+    redirect_to(:action => 'index')
+  end
+
+end
