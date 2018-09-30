eec1c2bdf8136c44ac5e7efc3bbf9e06086c483f
Jörgen Ivarsson <23526123+JorgenIvarsson@users.noreply.github.com>
2018-09-30 23:16:54 +0200

    minor adjustments of the comments in the controller subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -18,10 +18,8 @@ class SubjectsController < ApplicationController
     # if subject saved redirect to subjects list
     if @subject.save
       redirect_to(subjects_path)
-    # If save fails, just render the new template with the same @subject
     else
-      # render the New template. The New template is expecting @subject and that's what it's getting
-      # The form will have all parameters pre populated in the form thanks to the @subject.
+      # If save fails, just render the New template with the same @subject
       render('new')
     end
   end
