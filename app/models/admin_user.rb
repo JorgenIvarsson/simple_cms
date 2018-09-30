3c5fd785be1619e086d935cccbe44b9a6776bd31
Jörgen Ivarsson <23526123+JorgenIvarsson@users.noreply.github.com>
2018-09-30 15:32:34 +0200

    Updated rich association with relationship has_many through for sections and admin_users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,5 +2,8 @@ class AdminUser < ApplicationRecord
 
   has_and_belongs_to_many :pages
   has_many :section_edits
+  # Added rich associations. Relationship "has_many through" lets us use connection like
+  # "admin_user.sections"
+  has_many :sections, :through => :section_edits
 
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -2,5 +2,7 @@ class Section < ApplicationRecord
 
   belongs_to :page
   has_many :section_edits
-
+  # Added rich associations. Relationship "has_many through" lets us use connection like
+  # "section.admin_users"
+  has_many :admin_users, :through => :section_edits
 end
