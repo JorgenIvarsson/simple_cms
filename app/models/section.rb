384023615d117bbb2f4cb0c9c25cc92e51dbd23f
Jörgen Ivarsson <23526123+JorgenIvarsson@users.noreply.github.com>
2018-10-03 08:46:24 +0200

    Updated routes and scopes for Sections

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,4 +5,10 @@ class Section < ApplicationRecord
   # Added rich associations. Relationship "has_many through" lets us use connection like
   # "section.admin_users"
   has_many :admin_users, :through => :section_edits
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("position ASC") }
+  scope :newest_first, lambda { order("created_at DESC") }
+  scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"]) }
 end
