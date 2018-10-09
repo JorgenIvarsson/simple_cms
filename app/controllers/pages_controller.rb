ba70955008cb9ff68affec95743f27c2e9e24767
Jörgen Ivarsson <23526123+JorgenIvarsson@users.noreply.github.com>
2018-10-09 21:28:05 +0200

    Added links from subjects to pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
 class PagesController < ApplicationController
 
+  before_action :find_subject
   before_action :confirm_logged_in
 
   layout 'admin'
 
   def index
-    @pages = Page.sorted
+    @pages = @subject.pages.sorted
   end
 
   def show
@@ -13,7 +14,7 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new
+    @page = Page.new(:subject_id => @subject.id)
     @page_count = Page.count + 1
     @subjects = Subject.sorted
   end
@@ -23,7 +24,7 @@ class PagesController < ApplicationController
     #page_params is a local private metod for whitelisting and require params.
     if @page.save
       flash[:notice] = "Page created successfully."
-      redirect_to(pages_path)
+      redirect_to(pages_path(:subject_id => @subject.id))
     else
       @page_count = Page.count + 1
       @subjects = Subject.sorted
@@ -43,7 +44,7 @@ class PagesController < ApplicationController
     if @page.update_attributes(page_params)
       # if page is updated redirect to show action.
       flash[:notice] = "Page updated successfully."
-      redirect_to(page_path(@page))
+      redirect_to(page_path(@page, :subject_id => @subject.id))
     else
       # if save fails render template for edit.
       @page_count = Page.count
@@ -60,12 +61,16 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     @page.destroy
     flash[:notice] = "Page deleted successfully."
-    redirect_to(pages_path)
+    redirect_to(pages_path(:subject_id => @subject.id))
   end
 
   private
   # Whitelisting params to be mass assigned to an object. Also setting required to param :page
   def page_params
     params.required(:page).permit(:name, :position, :visible, :subject_id, :permalink)
+  end
+
+  def find_subject
+    @subject = Subject.find(params[:subject_id])
   end
 end
