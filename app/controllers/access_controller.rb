b132b3be45c0c347252a48c58436161b1272b2a8
Jörgen Ivarsson <jorgen.ivarsson@gmail.com>
2018-10-12 13:54:41 +0200

    Display logged in username

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -6,7 +6,7 @@ class AccessController < ApplicationController
 
   def menu
     # showing name of logged in user on admin page
-    @logged_in_user = AdminUser.find(session[:user_id]).name
+    @username = session[:username]
     # Display text and links
   end
 
@@ -25,6 +25,7 @@ class AccessController < ApplicationController
 
     if authorized_user
       session[:user_id] = authorized_user.id
+      session[:username] = authorized_user.username
       flash[:notice] = "You are logged in."
       redirect_to(admin_path)
     else
@@ -36,6 +37,7 @@ class AccessController < ApplicationController
 
   def logout
     session[:user_id] = nil
+    session[:username] = nil
     flash[:notice] = "Logged out"
     redirect_to(access_login_path)
   end
